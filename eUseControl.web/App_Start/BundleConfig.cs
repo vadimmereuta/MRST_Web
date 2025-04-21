7b847f3bbfc13a846c6f19f6af75e4526ff73532
Vadim_2004 <86158447+vadimmereuta@users.noreply.github.com>
2025-04-21 12:31:21 +0300

    v 0.0.2

---
+++ eUseControl.web/App_Start/BundleConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace eUseControl.web.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/Site.css",
+                "~/Content/bootstrap.min.css"));
+
+            //bundles.Add(new ScriptBundle("~/Scripts/js").Include(
+            //     "~/Scripts/jquery-1.10.2.min.js",
+            //     "~/Scripts/bootstrap.min.js"));
+
+
+
+            // Activează optimizările (bun pt. test și release)
+            BundleTable.EnableOptimizations = true;
+        }
+    }
+}
