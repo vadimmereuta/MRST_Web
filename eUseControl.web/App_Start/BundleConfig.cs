230a80b028d9d645710ab1fd072a2ad904a01ef0
Vadim_2004 <86158447+vadimmereuta@users.noreply.github.com>
2025-04-21 16:39:24 +0300

    0.0.3

--- eUseControl.web/App_Start/BundleConfig.cs
+++ eUseControl.web/App_Start/BundleConfig.cs
@@ -14,14 +14,28 @@ namespace eUseControl.web.App_Start
                 "~/Content/Site.css",
                 "~/Content/bootstrap.min.css"));
 
-            //bundles.Add(new ScriptBundle("~/Scripts/js").Include(
-            //     "~/Scripts/jquery-1.10.2.min.js",
-            //     "~/Scripts/bootstrap.min.js"));
+            bundles.Add(new ScriptBundle("~/bundles/js").Include(
+                "~/Scripts/jquery-2.0.0.min.js",
+                "~/Scripts/bootstrap.min.js"));
 
+            bundles.Add(new StyleBundle("~/bundles/bootstrap/css").Include(
+                "~/Content/bootstrap.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstap/js").Include(
+                "~/Scripts/bootstrap.min.js"));
+
+            bundles.Add(new StyleBundle("~/bundles/font-awesome/css").Include(
+                "~/Content/font-awesome.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/toaster/css").Include(
+                "~/Vendors/toastr/toastr.min.css", new CssRewriteUrlTransform()));
+
+            bundles.Add(new StyleBundle("~/bundles/datatables/css").Include(
+                "~/Vendors/datatables/datatables.min.css", new CssRewriteUrlTransform()));
 
 
             // Activează optimizările (bun pt. test și release)
-            BundleTable.EnableOptimizations = true;
+            BundleTable.EnableOptimizations = false;
         }
     }
 }
